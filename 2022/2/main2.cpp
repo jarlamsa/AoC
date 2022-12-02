9c16a3d376f49d138381aeb432a6be8d23a67523
Jarno Lämsä <jarno.lamsa@nordicsemi.no>
2022-12-02 09:29:35 +0200

    Added solution for day 2 of 2022

---
+++ 2022/2/main2.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+int countScore(char gnome, char me)
+{
+	int result = 0;
+
+	switch (me)
+	{
+	case 'X':
+		if (gnome == 'A')
+			result = 3;
+		else if (gnome == 'B')
+			result = 1;
+		else if (gnome == 'C')
+			result = 2;
+		break;
+	case 'Y':
+		if (gnome == 'A')
+			result = 4;
+		else if (gnome == 'B')
+			result = 5;
+		else if (gnome == 'C')
+			result = 6;
+		break;
+	case 'Z':
+		if (gnome == 'A')
+			result = 8;
+		else if (gnome == 'B')
+			result = 9;
+		else if (gnome == 'C')
+			result = 7;
+		break;
+	default:
+		break;
+	}
+	return result;
+}
+
+int main()
+{
+	ifstream infile("input.txt");
+
+	string buffer;
+	char gnome;
+	char me;
+	int score = 0;
+
+	while(getline(infile, buffer)) {
+		stringstream ss(buffer);
+		ss >> gnome;
+		ss >> me;
+		score += countScore(gnome, me);
+	}
+
+	cout << "Final score: " << score << endl;
+	return 0;
+}
